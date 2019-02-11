b03d315b6b7ab46c917febb40b9fbbebfefbbaad
Felix Förtsch <felixfoertsch@gmail.com>
2019-02-11 23:50:25 +0100

    add colors to asset to prepare for color pallette switching, create switch on colors

--- GitHub-Contributions-macOS/GitHub-Contributions-macOS/Sources/Helper/AppConfig.swift
+++ GitHub-Contributions-macOS/GitHub-Contributions-macOS/Sources/Helper/AppConfig.swift
@@ -2,4 +2,5 @@ import Foundation
 
 public struct AppConfig {
     public static let appGroup = "NG5W75WE8U.de.felixfoertsch.GitHub-Contributions"
+    public static let colorSet = "Colors/GitHub/"
 }
--- GitHub-Contributions-macOS/GitHub-Contributions-macOS/Sources/Model/Contribution.swift
+++ GitHub-Contributions-macOS/GitHub-Contributions-macOS/Sources/Model/Contribution.swift
@@ -11,7 +11,23 @@ public struct Contribution {
         let image = NSImage(size: NSSize(width: edgeLength, height: edgeLength))
         image.lockFocus()
         // TODO: Use color from contribution
-        let color = NSColor.red
+        
+        let color: NSColor
+        let colorSet = AppConfig.colorSet
+        
+        switch contribution.dataCount {
+        case 1...2:
+            color = NSColor(named: colorSet + "1")!
+        case 3...5:
+            color = NSColor(named: colorSet + "2")!
+        case 6...7:
+            color = NSColor(named: colorSet + "3")!
+        case 8...:
+            color = NSColor(named: colorSet + "4")!
+        default:
+            color = NSColor(named: colorSet + "0")!
+        }
+        
         color.setFill()
         NSBezierPath(rect: NSRect(x: 0.0, y: 0.0, width: edgeLength, height: edgeLength)).fill()
         image.unlockFocus()
