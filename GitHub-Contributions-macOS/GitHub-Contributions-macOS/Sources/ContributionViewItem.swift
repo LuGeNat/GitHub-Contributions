57cbd79f200a8ece7e0a7fdee8af204d32ee19f2
Felix Förtsch <felixfoertsch@gmail.com>
2019-02-25 20:32:18 +0100

    minimize the CustomCollectionView example

--- GitHub-Contributions-macOS/GitHub-Contributions-macOS/Sources/ContributionViewItem.swift
+++ GitHub-Contributions-macOS/GitHub-Contributions-macOS/Sources/ContributionViewItem.swift
@@ -1,14 +1,6 @@
 import Cocoa
 
 class ContributionViewItem: NSCollectionViewItem {
-
-    override func viewDidLoad() {
-        self.view = NSView()
-        super.viewDidLoad()
-    }
-    
     override func loadView() {
-        self.view = ContributionView()
     }
-    
 }
